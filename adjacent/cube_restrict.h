a448779f582f165c7c9f3c847552686648e3d7dd
shoko <choco@DESKTOP-FKLLD8M.localdomain>
2026-05-21 18:35:08 +0900

    制限を好きなcubeで作れるように変更

--- adjacent/cube_restrict.h
+++ adjacent/cube_restrict.h
@@ -7,5 +7,6 @@
 Cube* duplicate_list(const Cube* head);
 Cube* create_union_F_or_R(const Cube* list_10, const Cube* list_01);
 Cube* compute_restriction_optimized(const Cube* list_10, const Cube* list_01);
+Cube* create_universe_cube(void);
 
 #endif
