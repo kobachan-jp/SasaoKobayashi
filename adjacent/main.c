758d33ab7f6b931d439d16ef8476d62790646ca1
shoko <choco@DESKTOP-FKLLD8M.localdomain>
2026-05-21 18:36:04 +0900

    制限まで実行できるようにする

--- adjacent/main.c
+++ adjacent/main.c
@@ -6,24 +6,31 @@
 
 
 int main(void) {
-    // 1. システム（プール）の初期化
+    // 1. リスト初期化
     init_cube_pool();
 
     Cube* output_10_list = NULL;
     Cube* output_01_list = NULL;
     const char* filename = "test.pla";
 
-    // 2. 処理の実行
+    // 2. ファイル読み込み
     printf("Reading PLA file: %s ...\n", filename);
     if (read_pla_file(filename, &output_10_list, &output_01_list)) {
         printf("Successfully loaded PLA file!\n");
-
+        //10と01のリストがきちんと読み込まれているか確認.
         print_cube_list("Output 10 Group", output_10_list);
         print_cube_list("Output 01 Group", output_01_list);
-        
-        
+    }else{
+        printf("Cannot Read File\n");
+        exit(0);
     }
 
+    //3.制限を求める（9.4.1)
+    Cube* uni = create_universe_cube();
+    Cube* F_or_R = create_union_F_or_R(output_10_list,output_01_list);
+    Cube* G = compute_restriction_optimized(F_or_R, uni);
+    //4.Gの否定を求める(9.4.2)
+    
     // 3. 後片付け
     free_cube_list(output_10_list);
     free_cube_list(output_01_list);
